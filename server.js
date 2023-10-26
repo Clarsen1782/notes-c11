6b9ad9018e418102e4774cfd13607591c8754c82
Christina Larsen <clarsen1782@outlook.com>
2023-10-25 20:26:02 -0700

    Basic set up

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+  });
