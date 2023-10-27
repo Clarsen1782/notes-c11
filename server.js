6fa64332984779220b74080b9b1df54a8c6f7d32
Christina Larsen <clarsen1782@outlook.com>
2023-10-26 23:01:13 -0700

    Added Routes drive with api and html

--- server.js
+++ server.js
@@ -8,10 +8,16 @@ const PORT = 3001;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html')); 
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
 app.listen(PORT, () => {
