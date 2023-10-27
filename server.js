f3b7b4a9113934385c3274d2f44398b8405b9aab
Christina Larsen <clarsen1782@outlook.com>
2023-10-26 22:57:44 -0700

    working on server.js set up

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const router = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
 const path = require('path');
+const fs = require('fs')
 
 const PORT = 3001;
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html')); 
+});
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
