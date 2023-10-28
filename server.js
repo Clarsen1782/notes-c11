0241e2ed161039f83eca8bf6eebe124db8929007
Christina Larsen <clarsen1782@outlook.com>
2023-10-27 18:35:40 -0700

    wtf

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const api_routes = require('./api');
 const index_routes = require('./html');
 
 
-const PORT = process.env.Port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -14,6 +14,4 @@ app.use(api_routes);
 app.use(index_routes);
 
 
-app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`);
-  });
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
