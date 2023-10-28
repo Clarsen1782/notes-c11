641ddc9801bae613ba5dcf2678b5bd7fce30b762
Christina Larsen <clarsen1782@outlook.com>
2023-10-27 17:14:57 -0700

    Ready to Deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const index_routes = require('./html')
-const api_routes = require('./api')
+const api_routes = require('./api');
+const index_routes = require('./html');
+
 
 const PORT = 3001;
 
@@ -9,8 +10,9 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(express.static('public'));
-app.use(index_routes)
-app.use(api_routes)
+app.use(api_routes);
+app.use(index_routes);
+
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
